d5375016bafce8bd2ff9f92e8c899149911f5982
Akshay-1201 <akshaykute12@gmail.com>
2023-04-15 23:05:21 +0530

    script added

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('btn');
+
+const randomcolor = () => {
+    let val = '0123456789ABCDEF';
+    let con = '#';
+    for(let i = 0 ; i < 6 ; i++) {
+        con = con + val[Math.floor(Math.random() * 16)];
+    }
+}
+console.log(randomcolor);
+function changerandomcolor () {
+    document.body.style.backgroundColor = randomcolor();
+}
+button.addEventListener('click',changerandomcolor);
